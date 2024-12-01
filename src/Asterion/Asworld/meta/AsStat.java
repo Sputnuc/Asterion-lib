8473a0ccbc69dda96ba769ca9330ce9d3fd4f942
Sputnuc <asdasdasdboom123@gmail.com>
2024-12-01 23:24:30 +0500

    Create AsStat.java

---
+++ src/Asterion/Asworld/meta/AsStat.java
@@ -0,0 +1,20 @@
+package Asterion.Asworld.meta;
+
+
+
+import mindustry.world.meta.Stat;
+
+import mindustry.world.meta.StatCat;
+
+
+
+public class AsStat {
+
+    public static final Stat
+
+    reloadFrom = new Stat("reloadFrom",StatCat.function),
+
+    reloadTo = new Stat("reloadTo",StatCat.function);
+
+}
+
