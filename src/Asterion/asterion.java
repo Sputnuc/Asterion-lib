b5b88cddc53e67bf26465a5f40a6a382917e17a1
Sputnuc <asdasdasdboom123@gmail.com>
2024-12-01 23:16:14 +0500

    Update asterion.java

--- src/Asterion/asterion.java
+++ src/Asterion/asterion.java
@@ -1,1 +1,16 @@
+package Asterion;
+
+import Asterion.Ascontent.blocks;
+import arc.util.*;
+import mindustry.mod.*;
+
+public class asterion extends Mod{
+
+    @Override
+    public void loadContent(){
+        Log.info("Loading some Asterion content.");
+        blocks.load();
+    }
+
+}
 
